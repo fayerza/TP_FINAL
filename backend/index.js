84dfcd4ac1ae757b01d89b853c443ec35e0869b8
augusto <ajeflores@fi.uba.ar>
2025-07-21 19:44:30 -0300

    Regions endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 5000;
 
 const monstersRoutes= require('./app/routes/monsters_route.js');
+const regionRoutes = require('./app/routes/regions_route.js');
 
 app.use(express.json());
 app.use("/api/v1/monsters",monstersRoutes);
+app.use("/api/v1/regions",regionRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
