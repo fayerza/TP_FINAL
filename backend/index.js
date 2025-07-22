afc4dfa2e0c020deb5156a25aca441951ba98c53
augusto <ajeflores@fi.uba.ar>
2025-07-21 22:06:15 -0300

    Weapons endPoints

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ const port = 5000;
 
 const monstersRoutes= require('./app/routes/monsters_route.js');
 const regionRoutes = require('./app/routes/regions_route.js');
+const weaponsRoutes = require('./app/routes/weapons_route.js');
 
 app.use(express.json());
 app.use("/api/v1/monsters",monstersRoutes);
 app.use("/api/v1/regions",regionRoutes);
+app.use("/api/v1/weapons",weaponsRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
